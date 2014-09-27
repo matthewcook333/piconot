28f384018d9fc264e60e093328ca5138e837e902
ChloeCalvarin <chloe.calvarin@gmail.com>
2014-09-27 13:57:55 -0700

    Working code, just we can't solve picobot, for the empty room.

---
+++ src/main/scala/piconot/Empty.scala
@@ -0,0 +1,27 @@
+package piconot
+
+import java.io.File
+
+import picolib.maze.Maze
+import picolib.semantics.GUIDisplay
+import picolib.semantics.Picobot
+import picolib.semantics.TextDisplay
+import picolib.semantics.Rule
+
+import scalafx.application.JFXApp
+
+object Empty extends JFXApp {
+  val emptyMaze = Maze("resources" + File.separator + "empty.txt")
+  
+  val rules = List(
+	N arrow(F, R, B, L),
+	S arrow(F, L, B, R),
+	E arrow(R, L, F, B),
+	W arrow(L, R, F, B)
+  ).flatten
+  
+  object EmptyBot extends Picobot(emptyMaze, rules)
+  		with TextDisplay with GUIDisplay
+
+  stage = EmptyBot.mainStage
+}
